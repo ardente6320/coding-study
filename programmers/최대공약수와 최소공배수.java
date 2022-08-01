dc10c5af45843eed7be7ee13b52003320261cef2
Oliver <37868166+ardente6320@users.noreply.github.com>
2022-08-01 09:17:37 +0900

    최대공약수와 최소공배수
    level 1 연습문제

---
+++ programmers/최대공약수와 최소공배수.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    /**
+    * 최대 공약수 유클리드 호제법
+    */
+    private int GCD(int a, int b){
+        if(a % b == 0) 
+            return b;
+        return GCD(b, a % b);
+    }
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        answer[0] = GCD(Math.max(n,m),Math.min(n,m));
+        answer[1] = (n * m) / answer[0];
+        return answer;
+    }
+}
